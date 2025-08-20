5c043a8f6949080e7d44d097c0207e426a55a7b6
Mateus Sandes <mateus.sandes@fcxlabs.com.br>
2025-08-20 16:11:49 -0300

    Adicionado os mapeamentos do AutoMapper

--- src/Services/UserService.cs
+++ src/Services/UserService.cs
@@ -1,6 +1,7 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata;
 using StudyProject.Data.DTOs;
 using StudyProject.Models;
-using StudyProject.Repositories;
 using StudyProject.Repositories.Interfaces;
 
 namespace StudyProject.Services;
@@ -9,61 +10,39 @@ public class UserService
 {
     private readonly IUserRespoitory _userRepository;
     private readonly IPersonRepository _personRepository;
+    private readonly IMapper _mapper;
 
     public UserService(
         IUserRespoitory userRepository,
-        IPersonRepository personRepository)
+        IPersonRepository personRepository,
+        IMapper mapper)
     {
         _userRepository = userRepository;
         _personRepository = personRepository;
+        _mapper = mapper;
     }
 
     public async Task<IEnumerable<UserDTO>> FindAllUser()
     {
         var users = await _userRepository.FindAllUsers();
 
-        var usersDto = users.Select(u => new UserDTO
-        {
-            Id = u.Id,
-            Accessess = u.Accesses,
-            Login = u.Login,
-            Password = u.Password,
-            PersonId = u.Person!.Id,
-            DeviceId = u.Device != null ? u.Device.Id : 0
-        }).ToList();
-
-        return usersDto;
+        return _mapper.Map<IEnumerable<UserDTO>>(users);
     }
 
     public async Task<UserDTO> CreateUser(string personName, CreateUserDeviceDTO dto)
     {
         var foundPerson = await _personRepository.FindByName(personName);
 
-        var newUser = new User
+        var newUser = _mapper.Map<User>(dto, opt =>
         {
-            Login = dto.Login,
-            Password = dto.Password,
-            Accesses = dto.Accesses,
-            PersonId = foundPerson.Id,
-            Device = new Device
-            {
-                DeviceName = dto.createDevicedTO.DeviceName
-            }
-        };
+            opt.Items["PersonId"] = foundPerson.Id;
+        });
 
         foundPerson?.Users?.Add(newUser);
 
         var createdUser = await _userRepository.CreateUser(newUser);
 
-        var userDto = new UserDTO
-        {
-            Id = createdUser.Id,
-            Accessess = createdUser.Accesses,
-            Login = createdUser.Login,
-            Password = createdUser.Login,
-            PersonId = createdUser.PersonId,
-            DeviceId = createdUser.Device.Id
-        };
+        var userDto = _mapper.Map<UserDTO>(createdUser);
 
         return userDto;
     }
