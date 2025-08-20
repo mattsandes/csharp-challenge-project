883b74e9ef6ce7b7170bb8b1301b2822fa8637e5
Mateus Sandes <mateus.sandes@fcxlabs.com.br>
2025-08-20 16:09:00 -0300

    Corrigido o tipo do objeto createDeviceDTO

--- src/Models/DTOs/RequestDTOs/CreateUserDeviceDTO.cs
+++ src/Models/DTOs/RequestDTOs/CreateUserDeviceDTO.cs
@@ -5,5 +5,5 @@ public class CreateUserDeviceDTO
     public string Login { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public int Accesses { get; set; } = 0;
-    public createDevicedTO createDevicedTO { get; set; } = new();
+    public CreateDeviceDTO createDevicedTO { get; set; } = new();
 }
