15fb39d0a87c79cf1e0f52493bead8dd0a6b3994
Mateus Sandes <mateus.sandes@fcxlabs.com.br>
2025-08-20 16:06:03 -0300

    Alterada a propriedade createUsersDTO para Users

--- src/Models/DTOs/RequestDTOs/CreatePersonUserDTO.cs
+++ src/Models/DTOs/RequestDTOs/CreatePersonUserDTO.cs
@@ -3,5 +3,5 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 public class CreatePersonUserDTO
 {
     public string Name { get; set; } = string.Empty;
-    public List<CreateUserDTOs> CreateUserDTOs { get; set; } = new();
+    public List<CreateUserDTOs> Users { get; set; } = new();
 }
