a462caa9ba219397d79e189af9e783ccd9c4e762
Mateus Sandes <mateus.sandes@fcxlabs.com.br>
2025-08-20 16:10:24 -0300

    Removido o s excedente de Accesses

--- src/Models/DTOs/ResponseDTOs/UserDTO.cs
+++ src/Models/DTOs/ResponseDTOs/UserDTO.cs
@@ -5,7 +5,7 @@ public class UserDTO
     public long Id { get; set; }
     public string Login { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public int Accessess { get; set; } = 0;
+    public int Accesses { get; set; } = 0;
     public long PersonId { get; set; }
     public long? DeviceId { get; set; }
 }
